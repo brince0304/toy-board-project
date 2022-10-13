87d09d02115a6d65935f73aa6abd9a699d2f528b
brince0304 <brince0304@gmail.com>
2022-10-14 00:13:44 +0900

    #20- 메인 컨트롤러 작성

---
+++ src/test/java/com/fastcampus/projectboard/Controller/MainControllerTest.java
@@ -0,0 +1,31 @@
+package com.fastcampus.projectboard.Controller;
+
+import com.fastcampus.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform(get("/")).andExpect(status().is3xxRedirection());
+
+    }
+}
