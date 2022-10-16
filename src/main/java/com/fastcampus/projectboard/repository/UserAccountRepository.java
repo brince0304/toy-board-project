2eb30076b8f909456405012074e1afb4458b34dd
brince0304 <brince0304@gmail.com>
2022-10-16 13:13:55 +0900

    #23 - 연관관계 재설정에 따른 리포지토리 데이터 타입 변경
    Long 에서 String 으로 변경

--- src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount,String> {
 }
