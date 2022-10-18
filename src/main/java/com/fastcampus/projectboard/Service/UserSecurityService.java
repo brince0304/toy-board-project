5043aa5230d3f6b5375f36c541433eaa7c917f1a
brince0304 <brince0304@gmail.com>
2022-10-19 00:33:56 +0900

    #20 - 스프링 시큐리티 설정 추가
    로그인,로그아웃 기능 추가 / 유저 시큐리티 서비스 생성 및 검증 기능 구현 
    기존에 {noop}로 입력하던 비밀번호 기입 방식을 BCrypt 로 암호화 시키도록 변경함.
    configureGlobal 메소드를 선언하고 로그인 시에 비밀번호 입력값을 인코딩 해주도록 추가함으로써 로그인이 성공함.

---
+++ src/main/java/com/fastcampus/projectboard/Service/UserSecurityService.java
@@ -0,0 +1,47 @@
+package com.fastcampus.projectboard.Service;
+
+import com.fastcampus.projectboard.domain.UserAccount;
+import com.fastcampus.projectboard.domain.UserAccountRole;
+import com.fastcampus.projectboard.domain.forms.UserCreateForm;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserSecurityService implements UserDetailsService {
+    private final UserAccountRepository userAccountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+            Optional<UserAccount> _account = userAccountRepository.findById(userId);
+            if (_account.isEmpty()) {
+                throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
+            }
+            UserAccount account = _account.get();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            if ("admin".equals(userId)) {
+                authorities.add(new SimpleGrantedAuthority(UserAccountRole.ADMIN.getValue()));
+            } else {
+                authorities.add(new SimpleGrantedAuthority(UserAccountRole.USER.getValue()));
+            }
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return new User(account.getUserId(), account.getUserPassword(), authorities);
+        }
+}
