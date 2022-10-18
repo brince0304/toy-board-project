90d63dcca7312239ae85e40871b4e31ceec880fa
brince0304 <brince0304@gmail.com>
2022-10-19 00:59:49 +0900

    #26 - 폼 수정
    폼 수정 , DTO 수정

--- src/main/java/com/fastcampus/projectboard/domain/forms/ArticleForm.java
+++ src/main/java/com/fastcampus/projectboard/domain/forms/ArticleForm.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class ArticleForm {
-    @NotEmpty(message = "제목을 입력해주세요.")
+    @NotEmpty(message = "* 제목을 입력해주세요.")
     private String title;
-    @NotEmpty(message = "내용을 입력해주세요.")
-    @Size(min = 10, message = "내용은 10자 이상 입력해주세요.")
-    @Size(max = 10000, message = "내용은 10000자 이하로 입력해주세요.")
+
+    @Size(min = 10, message = "* 내용은 10자 이상 입력해주세요.")
+    @Size(max = 10000, message = "* 내용은 10000자 이하로 입력해주세요.")
     private String content;
 
     private String hashtag;
