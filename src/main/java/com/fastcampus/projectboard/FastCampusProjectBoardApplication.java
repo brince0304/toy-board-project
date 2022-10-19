41dc574ebd98c046056459457996c26e8ac15b42
brince0304 <brince0304@gmail.com>
2022-10-19 19:21:31 +0900

    #29 - 게시글 수정 뷰 구현

--- src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -3,6 +3,8 @@ package com.fastcampus.projectboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -11,5 +13,8 @@ public class FastCampusProjectBoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 	}
-
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
