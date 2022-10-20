d1a1bd8f29dc7766b76d57649cd901404fc1cd7c
brince0304 <brince0304@gmail.com>
2022-10-20 22:17:51 +0900

    #29 - 잡다한 수정
    으하아아암

--- src/main/java/com/fastcampus/projectboard/domain/forms/ArticleForm.java
+++ src/main/java/com/fastcampus/projectboard/domain/forms/ArticleForm.java
@@ -12,12 +12,10 @@ public class ArticleForm {
     @NotEmpty(message = "* 제목을 입력해주세요.")
     private String title;
 
-    @Size(min = 10, message = "* 내용은 10자 이상 입력해주세요.")
+    @Size(min = 5, message = "* 내용은 5자 이상 입력해주세요.")
     @Size(max = 10000, message = "* 내용은 10000자 이하로 입력해주세요.")
     private String content;
 
-    @Size(min= 1, max = 100, message = "* 댓글은 1자 이상 100자 이하로 작성해주세요.")
-    private String comment;
 
     private String hashtag;
 
