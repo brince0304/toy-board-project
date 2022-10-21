f9f727acbfb61f88e476e7cd7c2370731b33c4f3
brince0304 <brince0304@gmail.com>
2022-10-22 00:03:13 +0900

    데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,22 +5,6 @@ insert into article (id,user_id, created_at, created_by, modified_at, modified_b
 Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
 
 Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Teal', 'Charlotte''s Web');
-insert into article (id,user_id, created_at, created_by, modified_at, modified_by, content, hashtag, title) values (2,'test', '2022-01-31 03:12:59', 'Lisbeth', '2021-12-31 00:41:44', 'Babb', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
-
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Teal', 'Charlotte''s Web');
-insert into article (id,user_id, created_at, created_by, modified_at, modified_by, content, hashtag, title) values (3,'test', '2022-01-31 03:12:59', 'Lisbeth', '2021-12-31 00:41:44', 'Babb', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
-
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Teal', 'Charlotte''s Web');
-insert into article (id,user_id, created_at, created_by, modified_at, modified_by, content, hashtag, title) values (4,'test', '2022-01-31 03:12:59', 'Lisbeth', '2021-12-31 00:41:44', 'Babb', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
-
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Teal', 'Charlotte''s Web');
-
 
 insert into article_comment (id, created_at, created_by, modified_at, modified_by, content, user_id, article_id) values (1, '2022-10-11 21:48:43', 'Barde', '2021-11-04 11:55:27', 'Gibby', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
 
