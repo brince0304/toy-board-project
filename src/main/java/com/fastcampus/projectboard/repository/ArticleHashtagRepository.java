a3a2cb53688227bc7ccce8446af424fcd332f55e
brince0304 <brince0304@gmail.com>
2022-10-30 00:27:47 +0900

    #36 - 해시태그 repository 에 해시태그로 검색하는 추상 메소드 선언

--- src/main/java/com/fastcampus/projectboard/repository/ArticleHashtagRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleHashtagRepository.java
@@ -1,6 +1,7 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleHashtag;
+import com.fastcampus.projectboard.domain.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
@@ -9,5 +10,6 @@ import java.util.Set;
 public interface ArticleHashtagRepository extends JpaRepository<ArticleHashtag, Long> {
 
     Set<ArticleHashtag> findByArticleId(Long articleId);
+    Set<ArticleHashtag> findByHashtag(Hashtag hashtag);
 }
 
