7ce6d4562858fe8f2853530243d2bf7abc6bea68
brince0304 <brince0304@gmail.com>
2022-10-30 00:26:27 +0900

    #36 - 해시태그 검색시 게시물 반환하는 비즈니스 로직 설계
    해시태그를 검색하면 게시물을 반환하는 메소드를 선언하였다.

--- src/main/java/com/fastcampus/projectboard/Service/HashtagService.java
+++ src/main/java/com/fastcampus/projectboard/Service/HashtagService.java
@@ -1,7 +1,11 @@
 package com.fastcampus.projectboard.Service;
 
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.ArticleHashtag;
 import com.fastcampus.projectboard.domain.Hashtag;
+import com.fastcampus.projectboard.dto.ArticleDto;
 import com.fastcampus.projectboard.dto.HashtagDto;
+import com.fastcampus.projectboard.repository.ArticleHashtagRepository;
 import com.fastcampus.projectboard.repository.HashtagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,25 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Service
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
+@Service
 public class HashtagService {
 
     private final HashtagRepository hashtagRepository;
+    private final ArticleHashtagRepository articleHashtagRepository;
 
     public void saveHashtags(Set<HashtagDto> hashtags) {
         hashtags.stream()
                 .map(HashtagDto::hashtag)
                 .map(Hashtag::of)
-                .forEach(t-> {
-                    if(hashtagRepository.findByHashtag(t.getHashtag()).isEmpty()) {
+                .forEach(t -> {
+                    if (hashtagRepository.findByHashtag(t.getHashtag()).isEmpty()) {
                         hashtagRepository.save(t);
                     }
                 });
     }
-    public void saveHashtag(Hashtag hashtag){
+    public HashtagDto getHashtag(String hashtag) {
+        return HashtagDto.from(hashtagRepository.findByHashtag(hashtag).orElseThrow());
+    }
+
+    public void saveHashtag(Hashtag hashtag) {
         hashtagRepository.save(hashtag);
+    }
+
+    public Set<ArticleDto> getArticlesByHashtag(String hashtag) {
+        return hashtagRepository.findByHashtag(hashtag).get().getArticles().stream().map(t-> ArticleDto.from(t.getArticle())
+        ).collect(Collectors.toSet());
     }
 }
