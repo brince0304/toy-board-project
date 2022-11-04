e38b98bc77bd6bafa5cfe493727cdbf6809fe1a3
brince0304 <brince0304@gmail.com>
2022-11-04 19:43:16 +0900

    #43 - data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
 insert into user_account (created_at, created_by, modified_at, modified_by, email, memo, nickname, user_id, user_password) values ('2022-05-01 13:04:04', 'test', '2022-04-02 16:00:20', 'Adelaida', 'ablunt0@kickstarter.com', 'Vestibulum', 'test', 'test', '$2a$10$ESEkLtJ50G8ly/StrADSpOvqebjUwvCtY4/ZhtT2ClqO2jfGcSCCe');
 insert into user_account (created_at, created_by, modified_at, modified_by, email, memo, nickname, user_id, user_password) values ('2022-05-01 13:04:04', 'test2', '2022-04-02 16:00:20', 'Adelaida2', 'ablunt02@kickstarter.com', 'Vestibulum', 'test2', 'test2', '$2a$10$ESEkLtJ50G8ly/StrADSpOvqebjUwvCtY4/ZhtT2ClqO2jfGcSCCe');
 
+insert into user_account_roles(user_account_user_id, roles) values ('test', 'ROLE_USER');
+insert into user_account_roles(user_account_user_id, roles) values ('test2', 'ROLE_ADMIN');
+insert into user_account_roles(user_account_user_id, roles) values ('test2', 'ROLE_USER');
+
 insert into article (id,created_at, created_by, modified_at, modified_by, title, content, user_id,deleted) values (1,'2022-05-01 13:04:04', 'test', '2022-04-02 16:00:20', 'Adelaida', 'testtest', 'testesttesttest', 'test','N');
 
 insert into hashtag (id,hashtag) values (1,'text');
