0d7115114b1476c30aa9f51895a30e981e3fd04f
brince0304 <brince0304@gmail.com>
2022-12-11 20:00:48 +0900

    #59 - 불필요한 리포지토리 삭제

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.fastcampus.projectboard.repository;
-
-import com.fastcampus.projectboard.domain.ArticleComment;
-
-import java.util.Set;
-
-public interface ArticleCommentRepositoryCustom {
-    Set<ArticleComment> getChildrenCommentIsNotDeleted(Long id);
-}
