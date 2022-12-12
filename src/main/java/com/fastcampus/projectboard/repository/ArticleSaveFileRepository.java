f44ede25be89859c378843b3dd9e8ae523c83394
brince0304 <brince0304@gmail.com>
2022-12-12 19:18:04 +0900

    #61 - 리포지토리 불필요한 구문 삭제

--- src/main/java/com/fastcampus/projectboard/repository/ArticleSaveFileRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleSaveFileRepository.java
@@ -2,7 +2,6 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleSaveFile;
 
-import java.util.Optional;
 import java.util.Set;
 
 public interface ArticleSaveFileRepository extends org.springframework.data.jpa.repository.JpaRepository<com.fastcampus.projectboard.domain.ArticleSaveFile, java.lang.Long> {
@@ -10,7 +9,5 @@ public interface ArticleSaveFileRepository extends org.springframework.data.jpa.
 
     void deleteBySaveFileId(Long deletedFileId);
 
-    Optional<ArticleSaveFile> findBySaveFileId(Long saveFileId);
 
-    void deleteByArticleId(long articleId);
 }
