c936f47cb300ce3ec18a2d04fa8920cf79de6dc4
brince0304 <brince0304@gmail.com>
2022-12-12 19:17:13 +0900

    #61 - 에러메시지 추출

---
+++ src/main/java/com/fastcampus/projectboard/Messages/ErrorMessages.java
@@ -0,0 +1,32 @@
+package com.fastcampus.projectboard.Messages;
+
+public class ErrorMessages {
+    public static final String ENTITY_NOT_FOUND = "Entity Not Found";
+    public static final String ENTITY_ALREADY_EXISTS = "Entity Already Exists";
+    public static final String ENTITY_NOT_MATCH = "Entity Not Match";
+    public static final String ENTITY_NOT_VALID = "Entity Not Valid";
+    public static final String NOT_AUTHENTICATED = "Not Authenticated";
+    public static final String ACCOUNT_NOT_MATCH = "Account Not Match";
+
+    public static final String NOT_FOUND = "Not Found";
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String UNAUTHORIZED = "Unauthorized";
+    public static final String FORBIDDEN = "Forbidden";
+    public static final String METHOD_NOT_ALLOWED = "Method Not Allowed";
+    public static final String NOT_ACCEPTABLE = "Not Acceptable";
+
+    public static final String INVALID_INPUT_VALUE = "Invalid Input Value";
+
+    public static final String INVALID_TYPE_VALUE = "Invalid Type Value";
+
+    public static final String HANDLE_ACCESS_DENIED = "Access is Denied";
+
+    public static final String INVALID_PAGE_NUMBER = "Invalid Page Number";
+
+    public static final String INVALID_PAGE_SIZE = "Invalid Page Size";
+
+    public static final String ACCESS_TOKEN_NOT_FOUND = "Access Token Not Found";
+
+    public static final String REFRESH_TOKEN_NOT_FOUND = "Refresh Token Not Found";
+}
