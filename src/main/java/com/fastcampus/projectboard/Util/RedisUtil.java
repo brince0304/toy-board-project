3485c02085c9f9f82a58c4fa17f2886bc7a29e4a
brince0304 <brince0304@gmail.com>
2022-12-28 20:46:21 +0900

    #61 - 메소드명 수정

--- src/main/java/com/fastcampus/projectboard/Util/RedisUtil.java
+++ src/main/java/com/fastcampus/projectboard/Util/RedisUtil.java
@@ -1,5 +1,9 @@
 package com.fastcampus.projectboard.Util;
 
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -7,7 +11,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -21,26 +25,48 @@ public class RedisUtil {
 
     private final String EXPIRED_DURATION = "EXPIRE_DURATION";
 
-    public boolean isFirstIpRequest(String clientAddress, Long articleId) {
+    private final ObjectMapper mapper;
+
+    public boolean isFirstIpRequestForView(String clientAddress, Long articleId) {
         String key = generateViewKey(clientAddress, articleId);
         return !Boolean.TRUE.equals(stringRedisTemplate.hasKey(key));
     }
 
-    public void writeClientRequest(String clientAddress, Long articleId) {
+    public void writeClientRequestForView(String clientAddress, Long articleId) {
         String key = generateViewKey(clientAddress, articleId);
-        stringRedisTemplate.opsForValue().set(key, "id"+articleId);
+        stringRedisTemplate.opsForValue().set("LIKE_COUNT:"+key, "id"+articleId);
         stringRedisTemplate.expire(key, 60*60*24 , TimeUnit.SECONDS);
     }
-    public boolean isFirstIpRequest2(String clientAddress, Long articleId) {
+    public boolean isFirstIpRequestForLike(String clientAddress, Long articleId) {
         String key = generateLikeKey(clientAddress, articleId);
         return !Boolean.TRUE.equals(stringRedisTemplate.hasKey(key));
     }
 
-    public void writeClientRequest2(String clientAddress, Long articleId) {
+    public void writeClientRequestForLike(String clientAddress, Long articleId) {
         String key = generateLikeKey(clientAddress, articleId);
-        stringRedisTemplate.opsForValue().set(key, "id"+articleId);
+        stringRedisTemplate.opsForValue().set("VIEW_COUNT:"+key, "id"+articleId);
         stringRedisTemplate.expire(key, 60*60*24 , TimeUnit.SECONDS);
     }
+
+    public void saveDeletedArticle(Article.ArticleDto article) throws JsonProcessingException {
+        stringRedisTemplate.opsForValue().set("DELETED_ARTICLE: "+article.id(),mapper.writeValueAsString(article),365,TimeUnit.DAYS);
+    }
+
+    public void saveDeletedArticleComments(Set<ArticleComment.ArticleCommentDto> articleComments) throws JsonProcessingException {
+        articleComments.forEach(o->{
+            try {
+                stringRedisTemplate.opsForValue().set("DELETED_ARTICLE_COMMENT: "+o.id(), mapper.writeValueAsString(o),365,TimeUnit.DAYS);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public void saveDeletedArticleComment(ArticleComment.ArticleCommentDto articleComment) throws JsonProcessingException {
+        stringRedisTemplate.opsForValue().set("DELETED_ARTICLE_COMMENT: "+articleComment.id(),mapper.writeValueAsString(articleComment),365,TimeUnit.DAYS);
+    }
+
+
 
     // key 형식 : 'client Address + postId' ->  '\xac\xed\x00\x05t\x00\x0f127.0.0.1 + 500'
     private String generateViewKey(String clientAddress, Long articleId) {
